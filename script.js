2542a81d183614ff0222dc1fc05a9c5c78e62759
PedroPires25 <100771358+PedroPires25@users.noreply.github.com>
2025-05-05 15:51:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById("mensagem").textContent = "Este site usa HTML, CSS e JavaScript!";
+function mostrarSecao(id) {
+  const secoes = document.querySelectorAll('.secao');
+  secoes.forEach(secao => secao.classList.add('hidden'));
+
+  const ativa = document.getElementById(id);
+  if (ativa) {
+    ativa.classList.remove('hidden');
+  }
+}
